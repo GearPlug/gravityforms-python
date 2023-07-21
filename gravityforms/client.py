5fe68555318d25f405524aa48cccc1c30a9295a9
Juan <juankrios15@gmail.com>
2023-07-21 16:56:56 -0500

    added: requests base functions and list entries

--- gravityforms/client.py
+++ gravityforms/client.py
@@ -1,6 +1,59 @@
 import requests
+from requests_oauthlib import OAuth1
 
+from gravityforms.exceptions import UnauthorizedError, WrongFormatInputError, ContactsLimitExceededError
 
 class Client(object):
-    def __init__(self):
-        pass
+    def __init__(self, base_url, consumer_key, consumer_secret):
+        #TODO: Fix possible base_url different inputs.
+        self.URL = f"{base_url}wp-json/gf/v2/"
+        self.oauth = OAuth1(consumer_key, consumer_secret)
+
+    def list_entries(self):
+        return self.get("entries")
+
+    def get(self, endpoint, **kwargs):
+        response = self.request("GET", endpoint, **kwargs)
+        return self.parse(response)
+
+    def post(self, endpoint, **kwargs):
+        response = self.request("POST", endpoint, **kwargs)
+        return self.parse(response)
+
+    def delete(self, endpoint, **kwargs):
+        response = self.request("DELETE", endpoint, **kwargs)
+        return self.parse(response)
+
+    def put(self, endpoint, **kwargs):
+        response = self.request("PUT", endpoint, **kwargs)
+        return self.parse(response)
+
+    def patch(self, endpoint, **kwargs):
+        response = self.request("PATCH", endpoint, **kwargs)
+        return self.parse(response)
+
+    def request(self, method, endpoint, **kwargs):
+        return requests.request(method, self.URL + endpoint, auth=self.oauth, **kwargs)
+
+    def parse(self, response):
+        status_code = response.status_code
+        if "Content-Type" in response.headers and "application/json" in response.headers["Content-Type"]:
+            try:
+                r = response.json()
+            except ValueError:
+                r = response.text
+        else:
+            r = response.text
+        if status_code == 200:
+            return r
+        if status_code == 204:
+            return None
+        if status_code == 400:
+            raise WrongFormatInputError(r)
+        if status_code == 401:
+            raise UnauthorizedError(r)
+        if status_code == 406:
+            raise ContactsLimitExceededError(r)
+        if status_code == 500:
+            raise Exception
+        return r
