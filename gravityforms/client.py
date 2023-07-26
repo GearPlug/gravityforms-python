2c6434858ec8f7f7d423867b11ca29652861e233
Juan <juankrios15@gmail.com>
2023-07-26 14:56:12 -0500

    added: list and filter entries

--- gravityforms/client.py
+++ gravityforms/client.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from requests_oauthlib import OAuth1
 
@@ -7,10 +9,23 @@ class Client(object):
     def __init__(self, base_url, consumer_key, consumer_secret):
         #TODO: Fix possible base_url different inputs.
         self.URL = f"{base_url}wp-json/gf/v2/"
-        self.oauth = OAuth1(consumer_key, consumer_secret)
+        timestamp = str(int(time.time())+1000)
+        self.oauth = OAuth1(consumer_key, client_secret=consumer_secret, timestamp=timestamp)
 
     def list_entries(self):
         return self.get("entries")
+
+    def filter_entries(self, filter_field, filter_value, filter_operator, sorting_key=None, sorting_direction=None):
+        query = f'entries?search={{"field_filters": [{{"key":"{filter_field}","value":"{filter_value}","operator":"{filter_operator}"}}]}}'
+        if sorting_direction:
+            query += f"&sorting[direction]={sorting_direction}"
+        return self.get(query)
+
+    def list_forms(self):
+        return self.get("forms")
+
+    def get_form_detail(self, form_id):
+        return self.get(f"forms/{form_id}/results")
 
     def get(self, endpoint, **kwargs):
         response = self.request("GET", endpoint, **kwargs)
